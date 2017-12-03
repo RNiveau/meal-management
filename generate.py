09033640bc93114b893bd8c1ff4e66f0fe3b96de
Romain Niveau <romain.niveau@gmail.com>
2017-12-03 20:02:26 +0100

    Add facebook client

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@ from io import FileIO
 import logging
 
 from yaml import load
+from messager_client import MessagerClient
 
 import utils
 from engine import Engine
@@ -12,6 +13,7 @@ if __name__ == '__main__':
                         datefmt='%m/%d/%Y %I:%M:%S %p')
     logger = logging.getLogger(__name__)
     yaml = load(FileIO('./config.yaml', 'r'))
+    app = load(FileIO('./app.yaml', 'r'))
     dishes = utils.parse_dishes(yaml)
     side_dishes = utils.parse_side_dishes(yaml)
     week = utils.parse_calendar(yaml)
@@ -23,3 +25,5 @@ if __name__ == '__main__':
         logger.info("{}, {}, {}".format(day.name, day.evening_dish, day.evening_side_dish))
         if day.noon:
             logger.info("{}, {}, {}".format(day.name, day.noon_dish, day.noon_side_dish))
+    client = MessagerClient(app['facebook_id'])
+    client.send_message(app['sender_id'][0], 'toto')
